c39dc30bc54599364a2f895b127d78a59d0255db
Joao Victor <jov9051@outlook.com>
2023-01-23 19:37:04 -0300

    feat: core do projeto criado em app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [values, setValues] = useState<string[]>([]);
+  const [value, setValue] = useState<string>("");
+
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Valor gasto"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        ></input>
+        <button
+          type="submit"
+          onClick={() => {
+            setValues([...values, value]);
+            setValue("");
+          }}
+        >
+          Salvar
+        </button>
+      </div>
+
+      <div>
+        <ul>
+          {values.map((value) => {
+            return <li key={value}>${value}</li>;
+          })}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
