7c7e9b344a412ae1498ba0a4fe0347d9c8e76748
Joao Victor <jov9051@outlook.com>
2023-01-26 10:46:01 -0300

    feat: IHeaderProps adicionado a Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,22 @@
 import React from "react";
+import IHeaderProps from "../interfaces/IHeaderProps";
 
-export default function Header(): JSX.Element {
+export default function Header({ links }: IHeaderProps): JSX.Element {
   return (
     <header className="bg-dark w-full">
       <nav className="navbar navbar-expand">
         <div className="container">
           <h1 className="navbar-brand text-white">React Finance</h1>
           <ul className="navbar-nav mb-2">
-            <li className="nav-item">
-              <a className="nav-link text-white" href="#">
-                Nova Finança
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="#">
-                Finanças
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="#">
-                Editar Finança
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="#">
-                Deletar Finança
-              </a>
-            </li>
+            {links.map((link: string) => {
+              return (
+                <li className="nav-item" key={link}>
+                  <a className="nav-link text-white" href="#">
+                    {link}
+                  </a>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </nav>
