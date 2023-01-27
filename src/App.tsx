b6bf6aa5b000059b8a781d40831aebff98c0a97a
Joao Victor <jov9051@outlook.com>
2023-01-27 12:54:04 -0300

    feat: component Form criado

--- src/App.tsx
+++ src/App.tsx
@@ -2,77 +2,17 @@ import React, { useState } from "react";
 import Finance from "./utils/Finance";
 import Header from "./components/Header";
 import Errors from "./components/Errors";
-import { register, handleSubmit, reset, errors } from "./states/states";
+import { errors } from "./states/states";
 
 function App() {
   const [values, setValues] = useState<Finance[]>([]);
 
   return (
-    <div className="">
+    <div>
       <Header links={["Inicio", "Entradas", "Saidas", "Editar", "Deletar"]} />
       {errors.date && <Errors message={errors.date.message} />}
       {errors.value && <Errors message={errors.value.message} />}
       {errors.description && <Errors message={errors.description.message} />}
-      <form
-        onSubmit={handleSubmit((data) => {
-          setValues([
-            ...values,
-            new Finance(data.description, data.value, data.date),
-          ]);
-
-          reset();
-        })}
-        className="margin-auto"
-      >
-        <input
-          {...register("value", {
-            required: {
-              value: true,
-              message: "Valor é obrigatorio",
-            },
-            pattern: {
-              value: /[0-9]{1,5}$/,
-              message: "Valor Invalido",
-            },
-          })}
-          type="text"
-          placeholder="Valor gasto"
-        ></input>
-
-        <input
-          {...register("date", {
-            required: {
-              value: true,
-              message: "Data é obrigatorio",
-            },
-            pattern: {
-              value: /[0-9]{2,2}\/[0-9]{2,2}\/[0-9]{4,4}$/,
-              message: "Data Invalida",
-            },
-          })}
-          type="text"
-          placeholder="Data"
-        ></input>
-
-        <input
-          {...register("description", {
-            required: {
-              value: true,
-              message: "Descrição é obrigatorio",
-            },
-            pattern: {
-              value: /[a-z][A-Z]{1,100}$/gi,
-              message: "Descrição invalida",
-            },
-          })}
-          type="text"
-          placeholder="Descrição"
-        ></input>
-
-        <button className="btn btn-primary" type="submit">
-          Salvar
-        </button>
-      </form>
       <div>
         <ul>
           {values.map((value) => {
