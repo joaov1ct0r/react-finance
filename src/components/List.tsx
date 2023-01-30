a9603cbd5188a2f46a8560f1d465879b5c966cf8
Joao Victor <jov9051@outlook.com>
2023-01-30 20:15:07 -0300

    refactor: CardNegative

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -12,8 +12,8 @@ export default function List(): JSX.Element {
   const dispatch: AppDispatch = useDispatch();
 
   return (
-    <table className=" table bg-white w-75 mt-3 border border-dark">
-      <thead>
+    <table className="table bg-white w-75 mt-3 border border-dark">
+      <thead className="text-center">
         <tr>
           <th scope="col">Valor</th>
           <th scope="col">Data</th>
@@ -25,7 +25,7 @@ export default function List(): JSX.Element {
       <tbody>
         {finances.map((finance: Finance) => {
           return finance.type === "negative" ? (
-            <tr className="bg-danger text-white" key={finance.id}>
+            <tr className="text-center text-danger" key={finance.id}>
               <td>R${finance.value}</td>
               <td>{finance.date}</td>
               <td>{finance.description}</td>
@@ -45,7 +45,7 @@ export default function List(): JSX.Element {
               </td>
             </tr>
           ) : (
-            <tr className="bg-success text-white" key={finance.id}>
+            <tr className="text-center text-success" key={finance.id}>
               <td>R${finance.value}</td>
               <td>{finance.date}</td>
               <td>{finance.description}</td>
