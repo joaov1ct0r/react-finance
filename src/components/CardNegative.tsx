e4541e0b4b3fd352a846fa6f7a7435e0cc2f9c08
Joao Victor <jov9051@outlook.com>
2023-02-01 16:16:48 -0300

    feat: arrow adicionado a cardNegative

--- src/components/CardNegative.tsx
+++ src/components/CardNegative.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { ReduxState } from "../store";
 import Finance from "../utils/Finance";
+import { FiArrowDownCircle } from "react-icons/fi";
 
 export default function CardNegative(): JSX.Element {
   const finances: Finance[] = useSelector((state: ReduxState) => {
@@ -14,8 +15,9 @@ export default function CardNegative(): JSX.Element {
 
   return (
     <div className="card w-25 bg-danger text-center">
-      <div className="card-header">
+      <div className="card-header d-flex justify-content-between align-items-center">
         <h5 className="card-title text-white">Saidas</h5>
+        <FiArrowDownCircle size={30} color="#FFF"></FiArrowDownCircle>
       </div>
       <div className="card-body">
         <p className="card-text text-white text-center fs-5 text">R$ {total}</p>
