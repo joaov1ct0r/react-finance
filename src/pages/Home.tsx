488536ed7ccf43c0e2754bf9b2cf11f7722c10b7
Joao Victor <jov9051@outlook.com>
2023-02-03 18:39:16 -0300

    feat: page Home criado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CardTotal from "../components/CardTotal";
 import CardPositive from "../components/CardPositive";
 import CardNegative from "../components/CardNegative";
 import List from "../components/List";
 import ModalButton from "../components/ModalButton";
 import Modal from "../components/Modal";
+import { useDispatch } from "react-redux";
+import { createFinance } from "../store/reducers/financeSlicer";
+import Finance from "../utils/Finance";
 
 export default function Home() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(
+      createFinance({
+        description: "teste2",
+        value: 100,
+        date: "15/09/2001",
+      } as Finance)
+    );
+  }, []);
   return (
     <div>
       <div className="d-flex justify-content-evenly mt-5">
