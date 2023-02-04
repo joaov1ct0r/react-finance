259b8e975087f16ac5d133ef2c59cebf2b6e297d
Joao Victor <jov9051@outlook.com>
2023-02-04 20:47:34 -0300

    feat: component ListPostive criado

---
+++ src/components/ListPositive.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, ReduxState } from "../store";
+import Finance from "../utils/Finance";
+import { deleteFinance } from "../store/reducers/financeSlicer";
+import { FiTrash } from "react-icons/fi";
+
+export default function ListPositive(): JSX.Element {
+  const finances: Finance[] = useSelector((state: ReduxState) => {
+    return state;
+  });
+
+  const dispatch: AppDispatch = useDispatch();
+
+  return (
+    <table className="table bg-white w-75 mt-3 border border-dark">
+      <thead className="text-center">
+        <tr>
+          <th scope="col">Valor</th>
+          <th scope="col">Data</th>
+          <th scope="col">Descrição</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {finances.map((finance: Finance) => {
+          return finance.type === "positive" ? (
+            <tr
+              className="text-center text-success"
+              key={finance.id}
+              id={finance.id}
+            >
+              <td>
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                }).format(finance.value)}
+              </td>
+              <td>{finance.date}</td>
+              <td>{finance.description}</td>
+              <td>
+                <button
+                  id={finance.id}
+                  className="border border-white bg-danger text-white"
+                  onClick={(e: React.MouseEvent) => {
+                    e.preventDefault();
+
+                    console.log(e.currentTarget.parentElement?.parentElement);
+
+                    dispatch(deleteFinance(e.currentTarget.id));
+                  }}
+                >
+                  <FiTrash size={20}></FiTrash>
+                </button>
+              </td>
+            </tr>
+          ) : (
+            <></>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
