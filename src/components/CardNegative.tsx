3c0dc3319a6ca77089bd36c56ed4ccb0406df534
Joao Victor <jov9051@outlook.com>
2023-02-04 14:01:18 -0300

    refactor: cardNegative

--- src/components/CardNegative.tsx
+++ src/components/CardNegative.tsx
@@ -10,8 +10,15 @@ export default function CardNegative(): JSX.Element {
   });
 
   const total: number = finances.reduce((acc: number, index: Finance) => {
-    return index.value < 0 ? acc + index.value : acc + 0;
+    return index.value < 0 ? acc + index.value : acc;
   }, 0);
+
+  const formatedTotal = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(total);
 
   return (
     <div className="card w-25 bg-danger text-center">
@@ -21,12 +28,7 @@ export default function CardNegative(): JSX.Element {
       </div>
       <div className="card-body">
         <p className="card-text text-white text-center fs-5 text">
-          {new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2,
-          }).format(total)}
+          {formatedTotal}
         </p>
       </div>
     </div>
