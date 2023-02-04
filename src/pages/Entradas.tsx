340db87158438f2b5f60fba7765fd60c3763abd3
Joao Victor <jov9051@outlook.com>
2023-02-04 20:48:37 -0300

    feat: ListPositive adicionado a Entradas

--- src/pages/Entradas.tsx
+++ src/pages/Entradas.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Header from "../components/Header";
 import CardPositive from "../components/CardPositive";
-import ListEach from "../components/ListEach";
 import ModalButton from "../components/ModalButton";
 import Modal from "../components/Modal";
+import ListPositive from "../components/ListPositive";
 
 export default function Entradas(): JSX.Element {
   return (
@@ -13,7 +13,7 @@ export default function Entradas(): JSX.Element {
         <CardPositive />
       </div>
       <div className="d-flex justify-content-center mt-5">
-        <ListEach type="positive" />
+        <ListPositive />
       </div>
       <ModalButton />
       <Modal />
