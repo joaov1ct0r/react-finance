17a5ab2d0ec16903eb2a647ce419df2e7967c06a
Joao Victor <jov9051@outlook.com>
2023-02-06 18:35:12 -0300

    feat: hook useAppDispatch criado

---
+++ src/hooks/useAppDispatch.ts
@@ -0,0 +1,8 @@
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../store";
+
+export default function useAppDispatch() {
+  const dispatch: AppDispatch = useDispatch();
+
+  return { dispatch };
+}
